5f8727aa09202922c8b9978e0a513dc6dceea8dd
MatteZani <matteozaniboni95@gmail.com>
2024-03-12 17:09:51 +0100

    Classi iniziali

---
+++ Scenario.java
@@ -0,0 +1,29 @@
+public class Scenario {
+    private String description;
+    //private List<Option> options;
+
+    // Costruttore
+    public Scenario(String description){ //,List<Option> options) {
+        this.description = description;
+        //this.options = options;
+    }
+
+    // Metodi getter e setter
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /*public List<Option> getOptions() {
+        return options;
+    }
+
+    public void setOptions(List<Option> options) {
+        this.options = options;
+    }*/
+
+}
+
---
+++ Story.java
@@ -0,0 +1,40 @@
+import java.util.List;
+
+public class Story {
+    private String title;
+    private String description;
+    private List<Scenario> scenarios;
+
+    // Costruttore
+    public Story(String title, String description, List<Scenario> scenarios) {
+        this.title = title;
+        this.description = description;
+        this.scenarios = scenarios;
+    }
+
+    // Metodi getter e setter
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Scenario> getScenarios() {
+        return scenarios;
+    }
+
+    public void setScenarios(List<Scenario> scenarios) {
+        this.scenarios = scenarios;
+    }
+}
+

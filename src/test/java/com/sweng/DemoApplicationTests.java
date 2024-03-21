af090349014f937cda809969d82869d23c8c76db
MatteZani <matteozaniboni95@gmail.com>
2024-03-21 10:31:29 +0100

    UC1-Task3-Test Utente con dati mancanti

--- src/test/java/com/sweng/DemoApplicationTests.java
+++ src/test/java/com/sweng/DemoApplicationTests.java
@@ -5,12 +5,9 @@ import com.sweng.entity.User;
 import com.sweng.utilities.DBHandler;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.Assert;
 
 import static org.mockito.Mockito.when;
 
@@ -21,7 +18,7 @@ class RegistrationTest {
 	private DBHandler dbHandler;
 
 	@Test
-	void utenteCreatoCorrettamente(){
+	void userCorrectlyCreated() {
 
 		//given
 		User expected = new User(1, "Pippo", "Franco");
@@ -37,17 +34,22 @@ class RegistrationTest {
 	}
 
 	@Test
-	void eccezioneInCasoDiUserInvalido(){
+	void userMissingData() {
 
 		//given
 		User invalidUser = new User(1, null, null);
 		final int expectedStatusCode = 400;
 
+		// Configure mock behavior
+		when(dbHandler.saveUser(invalidUser)).thenReturn(new ResponseEntity<>(null, HttpStatus.BAD_REQUEST));
+
 		//when
+		ResponseEntity<Object> response = dbHandler.saveUser(invalidUser);
 
 		//then
-		Assertions.assertEquals(expectedStatusCode, dbHandler.saveUser(invalidUser).getStatusCode().value());
+		Assertions.assertEquals(expectedStatusCode, response.getStatusCode().value());
 
 	}
+
 
 }

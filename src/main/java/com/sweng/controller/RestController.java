5a5bbb136433487180d83e4256eb42708ce6c4bb
MatteZani <matteozaniboni95@gmail.com>
2024-03-28 08:46:18 +0100

    Revisione Rest controller

--- src/main/java/com/sweng/controller/RestController.java
+++ src/main/java/com/sweng/controller/RestController.java
@@ -1,8 +1,9 @@
-/*
 package com.sweng.controller;
 
 import com.sweng.entity.*;
 import com.sweng.utilities.DBHandler;
+import com.sweng.utilities.StoryService;
+import com.sweng.utilities.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,77 +16,71 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
-@org.springframework.web.bind.annotation.RestController
-@RequestMapping("/api/home")
-public class RestController {
+    @org.springframework.web.bind.annotation.RestController
+    @RequestMapping("/api/home")
+    public class RestController {
 
 
-    @Autowired
-    private DBHandler dbHandler;
+        @Autowired
+        private UserService userService;
+        @Autowired
+        private StoryService storyService;
 
-    Logger logger = LoggerFactory.getLogger(RestController.class);
+        Logger logger = LoggerFactory.getLogger(RestController.class);
 
-*/
-/*    @GetMapping("/user")
-    public ResponseEntity<List<Map<String, Object>>> getUsers() {
-        List<Map<String, Object>> users = dbHandler.getUsers();
-        return new ResponseEntity<>(users, HttpStatus.OK);
-    }*//*
-
-
-//    @GetMapping("/user/username")
-//    public ResponseEntity<Object> getUserByName(@RequestParam String username){
-////        if(username.equals("pippo")){
-////            User pippo = new User("pippo", "pippo");
-////            return new ResponseEntity<>(pippo, HttpStatus.OK);
-////        }
-////        else{
-////            return new ResponseEntity<>("Utente non trovato", HttpStatus.NOT_FOUND);
-////        }
-//    }
-
-    */
-/**
-     * inserisce un nuovo utente nel database
-     * @param user l'utente da inserire
-     * @return response entity con codice 200 e l'utente nel body se l'inserimento è andato a buon fide
-     * response entity 400 con messaggio di errore se si verifica un errore nell'inserimento
-     *
-     **//*
-
-*/
-/*    @PostMapping("/user")
-    public ResponseEntity<Object> postUser(@RequestBody User user){
-
-        return dbHandler.saveUser(user);
-    }*//*
-
-
-    /*@GetMapping("/story")
-    public ResponseEntity<Object> getStories(){
-
-
-        List<Story> stories = dbHandler.getStories();
-
-        if(stories == null) {
-            return new ResponseEntity<>("Errore nel recupero delle storie", HttpStatusCode.valueOf(400));
+        @GetMapping("/user")
+        public ResponseEntity<List<Map<String, Object>>> getUsers() {
+            List<Map<String, Object>> users = userService.getUsers();
+            return new ResponseEntity<>(users, HttpStatus.OK);
         }
-        else
-            return new ResponseEntity<>(stories, HttpStatus.OK);
-    }*/
 
-    /*@PostMapping("/story")
-    public ResponseEntity<Object> postStory(@RequestBody Story story){
-
-        try {
-            dbHandler.createStory(story);
-            return new ResponseEntity<>(story, HttpStatus.OK);
-        } catch (DataAccessException e) {
-            return new ResponseEntity<>("Errore nel salvataggio della storia", HttpStatusCode.valueOf(400));
+        @GetMapping("/user/username")
+        public ResponseEntity<Object> getUserByName(@RequestParam String username) {
+            if (username.equals("pippo")) {
+                User pippo = new User("pippo", "pippo");
+                return new ResponseEntity<>(pippo, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>("Utente non trovato", HttpStatus.NOT_FOUND);
+            }
         }
+
+        /**
+         * inserisce un nuovo utente nel database
+         *
+         * @param user l'utente da inserire
+         * @return response entity con codice 200 e l'utente nel body se l'inserimento è andato a buon fide
+         * response entity 400 con messaggio di errore se si verifica un errore nell'inserimento
+         **/
+        @PostMapping("/user")
+        public ResponseEntity<Object> postUser(@RequestBody User user) {
+            return userService.saveUser(user);
+        }
+
+        @GetMapping("/story")
+        public ResponseEntity<Object> getStories(){
+            List<Story> stories = storyService.getStories();
+
+            if(stories == null)
+                return new ResponseEntity<>("Errore nel recupero delle storie", HttpStatusCode.valueOf(400));
+            else
+                return new ResponseEntity<>(stories, HttpStatus.OK);
+        }
+
+        @PostMapping("/story")
+        public ResponseEntity<Object> postStory(@RequestBody Story story){
+
+            try {
+                storyService.createStory(story);
+                return new ResponseEntity<>(story, HttpStatus.OK);
+            } catch (DataAccessException e) {
+                return new ResponseEntity<>("Errore nel salvataggio della storia", HttpStatusCode.valueOf(400));
+            }
+        }
+
     }
 
-    @PostMapping("/object")
+
+   /*@PostMapping("/object")
     public ResponseEntity<Object> postObject(@RequestBody String name, @RequestBody String description){
 
         try {

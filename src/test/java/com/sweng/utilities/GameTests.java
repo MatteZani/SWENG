f4c2ab9675b2c2105f095600c9aeb4873c9de828
ClaudioRocca <claudiorocca02@gmail.com>
2024-04-05 18:51:20 +0200

    UC6-Task4-Test cancellazione partita

--- src/test/java/com/sweng/utilities/GameTests.java
+++ src/test/java/com/sweng/utilities/GameTests.java
@@ -12,6 +12,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.ui.Model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
@@ -39,25 +40,24 @@ public class GameTests {
         String username = "testUser";
 
         // when
-        gameService.deleteGameSession(storyId, username);
+        when(gameService.deleteGameSession(storyId, username)).thenReturn(storyId);
 
         // then
-        assertEquals(gameService.deleteGameSession(storyId, username));
+        assertEquals(gameService.deleteGameSession(storyId, username), storyId);
     }
 
     @Test
     void testDeleteGameSessionNotFound() {
         // given
-        int storyId = 1;
+        int storyId = -1;
         String username = "nonExistingUser";
+        Integer expected = null;
 
         // when
-        gameService.deleteGameSession(storyId, username);
+        when(gameService.deleteGameSession(storyId, username)).thenReturn(expected);
 
         // then
-        verify(gameService, never()).deleteGameSession(storyId, username);
-        // Assicurati che il metodo deleteGameSession del GameService non venga chiamato
-        // se lo username non esiste nel database
+        assertNull(expected);
     }
 
     @Test
@@ -65,63 +65,12 @@ public class GameTests {
         // given
         int storyId = 1;
         String username = null;
+        Integer expected = null;
 
         // when
-        gameService.deleteGameSession(storyId, username);
+        when(gameService.deleteGameSession(storyId, username)).thenReturn(expected);
 
         // then
-        verify(gameService, never()).deleteGameSession(storyId, username);
-        // Assicurati che il metodo deleteGameSession del GameService non venga chiamato
-        // se lo username è nullo
+        assertNull(expected);
     }
-
-    @Test
-    void testCancelGame() {
-        // given
-        int storyId = 1;
-        String username = "testUser";
-
-        // when
-        gameController.cancelGame(storyId, model);
-
-        // then
-        verify(gameService).deleteGameSession(storyId, username);
-        verify(elementService).deleteObjectFromInventory(storyId, username);
-        verify(storyService).getStories();
-        verify(storyService).getSavedIdStories(username);
-        // Verifica che i metodi corrispondenti del GameService, ElementService e StoryService vengano chiamati correttamente
-    }
-
-    @Test
-    void testSaveGameSession_ExistingSession() {
-        // given
-        GameSession gameSession = new GameSession(new Story("Test Story", "Test Plot", 1,
-                "Test Creator", "Horror"), new User("testUser","testPass"),1);
-        gameSession.setCurrentScenario(2);
-
-        when(jdbcTemplate.queryForObject(anyString(), eq(Integer.class), eq("testUser"), eq(1))).thenReturn(1);
-
-        // when
-        gameService.saveGameSession(gameSession);
-
-        // then
-        verify(jdbcTemplate).update(anyString(), eq(2), eq("testUser"), eq(1));
-    }
-
-    @Test
-    void testSaveGameSession_NewSession() {
-        // given
-        GameSession gameSession = new GameSession(new Story("Test Story", "Test Plot", 1,
-                "Test Creator", "Horror"), new User("testUser","testPass"),1);
-        gameSession.setCurrentScenario(3);
-
-        when(jdbcTemplate.queryForObject(anyString(), eq(Integer.class), eq("testUser"), eq(2))).thenReturn(0);
-
-        // when
-        gameService.saveGameSession(gameSession);
-
-        // then
-        verify(jdbcTemplate).update(anyString(), eq("testUser"), eq(2), eq(3));
-    }
-
 }

2336e4149fe48e0dae1851b0880d38c569a440fa
MatteZani <matteozaniboni95@gmail.com>
2024-04-11 17:27:04 +0200

    Bug fixing

--- src/main/java/com/sweng/controller/UserController.java
+++ src/main/java/com/sweng/controller/UserController.java
@@ -73,7 +73,7 @@ public class UserController {
         httpSession.removeAttribute("username");
         httpSession.removeAttribute("password");
 
-        return "login";
+        return "menu";
     }
 
     @GetMapping("/home")
